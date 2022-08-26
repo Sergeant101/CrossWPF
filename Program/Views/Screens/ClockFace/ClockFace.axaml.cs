e191519ad654a68cc48b87510ada7a5e62fb0689
Sergeant101 <s.v.zagorodnev@gmail.com>
2022-08-26 13:38:11 +0500

    Adding clock face

---
+++ Program/Views/Screens/ClockFace/ClockFace.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Program.ViewModels
+{
+    public partial class ClockFace : UserControl
+    {
+        public ClockFace()
+        {
+            InitializeComponent();
+        }
+    }
+}
